786597c5179ddd7ad9765bc39a91e5f3f7149ffb
Nahi002 <nahomdebele002@gmail.com>
2024-08-10 23:42:27 +0300

    app module

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -5,11 +5,12 @@ import { CategoryModule } from './modules/catagory/catagory.module';
 import { BookModule } from './modules/books/books.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { SalesModule } from './modules/sale/sales.module';
 
 
 @Global()
 @Module({
-  imports: [ UsersModule , CategoryModule , BookModule],
+  imports: [ UsersModule , CategoryModule , BookModule , SalesModule],
 
 })
 export class CoreModule {}
