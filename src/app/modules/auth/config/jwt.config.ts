6cca77e00aef25c67f5773d4aa6b79354cfff889
Nahi002 <nahomdebele002@gmail.com>
2024-08-10 23:42:00 +0300

    auth config

--- src/app/modules/auth/config/jwt.config.ts
+++ src/app/modules/auth/config/jwt.config.ts
@@ -5,10 +5,10 @@ export default registerAs("jwt", () => {
     secret: process.env.JWT_SECRET,
     audience: process.env.JWT_TOKEN_AUDIENCE,
     issuer: process.env.JWT_TOKEN_ISSUER,
-    accessTokenTtl: parseInt(process.env.JWT_ACCESS_TOKEN_TTL ?? "3600", 10),
+    accessTokenTtl: parseInt(process.env.JWT_ACCESS_TOKEN_TTL ?? "86400", 10),
     refreshTokenTtl: parseInt(process.env.JWT_REFRESH_TOKEN_TTL ?? "86400", 10),
     signOptions: {
-      expiresIn: process.env.JWT_EXPIRES_IN || '1h',
+      expiresIn: process.env.JWT_EXPIRES_IN || '5h',
     },
   };
 });
