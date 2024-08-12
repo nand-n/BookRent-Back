1245d2c9b0daacc0fa077448fbcea687f56600e9
Nahi002 <nahomdebele002@gmail.com>
2024-08-12 23:36:15 +0300

    user entity

--- src/app/modules/users/entities/user.entity.ts
+++ src/app/modules/users/entities/user.entity.ts
@@ -1,8 +1,9 @@
 import { BaseModel } from '../../../../database/base.model';
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, OneToMany } from 'typeorm';
 
 import { Role } from '../enums/role.enum';
 import { Permission, PermissionType } from '../../auth/autherization/permission.type';
+import { Book } from '../../books/entities/book.entity';
 
 @Entity()
 export class User extends BaseModel {
@@ -20,12 +21,20 @@ export class User extends BaseModel {
   @Column({ length: 50, type: 'varchar' ,nullable:true , unique:true})
   phone: string;
 
+  @Column({ type: 'boolean', default: false })
+  status: boolean;
+
   @Column({ length: 50, type: 'varchar', nullable:true , unique:true })
   email: string;
 
   @Column({ length: 50, type: 'varchar', nullable:true , unique:true })
   telegramUser: string;
 
+  @OneToMany(() => Book, (book) => book.user)
+  books: Book[];
+
+  @Column({  type: 'varchar', nullable:true  })
+  profilePicture: string;
 
 
 }
